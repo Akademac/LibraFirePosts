e8ba3e469a7aaba849b39f43a9151dd94bda4295
Djuki <gdjuric1989@gmail.com>
2022-04-01 16:26:18 +0200

    second commit

--- posts.js
+++ posts.js
@@ -1,4 +1,14 @@
 let x = window.location.hash;
-let y = x.substring(1, 2)
+let y = x.substring(1, 2);
 
-console.log(y);
+fetch(`https://jsonplaceholder.typicode.com/posts/${y}`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.error(err);
+  })
+
